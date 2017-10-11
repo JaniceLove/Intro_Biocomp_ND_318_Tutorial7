8c226bb607e44a2b8c408cc923c292f46aed62b8
Melissa Stephens <mpullins@nd.edu>
2017-10-11 16:46:16 -0400

    working on the for loop to calculate GC conent and sequence length by appending them to lists

--- lecture11histogram.py
+++ lecture11histogram.py
@@ -12,3 +12,16 @@ seqID=[]
 #loop through each line of the file, assigning the seqID and seqlength to the lists
 #loop through and count the number of Gs and Cs
 
+for line in Infile:
+	line=line.strip()
+	if '>' in line:
+		seqID.append(line)
+	else:
+		Slength=float(len(line))
+		Gcount=line.count("G")
+		Ccount=line.count("C")
+	seqlength.append(Slength)
+	percentGC.append((Gcount+Ccount)/Slength*100)
+
+# make lists into a dataframe that you can use to plot the histogram
+data=pandas.DataFrame(list(seqID,percentGC,seqlength),columns=['SequenceID,'SequenceLength','PercentGC'])
