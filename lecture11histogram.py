1e3df4e38e4dfe5c18e1a4c0bd573acc7b9268cd
Melissa Stephens <mpullins@nd.edu>
2017-10-11 21:12:30 -0400

    got for loop working and combined lists into dataframe for histograms

--- lecture11histogram.py
+++ lecture11histogram.py
@@ -1,6 +1,8 @@
 #Generate a histogram of sequence lengths and GC content using the Lecture11.fasta file
 #load data
+import pandas
 Infile=open("Lecture11.fasta","r")
+all_lines=Infile.readlines()
 
 #create lists for storing information
 seqlength=[]
@@ -8,22 +10,26 @@ percentGC=[]
 seqID=[]
 Gcount=0
 Ccount=0
+Slength=0
 
 #need to count the number of characters (sequence length)
 #need to count the number of Gs and Cs and divide by length (G,C/# of characters)x 100%
 #loop through each line of the file, assigning the seqID and seqlength to the lists
 #loop through and count the number of Gs and Cs
 
-for line in Infile:
+for line in all_lines:
 	line=line.strip()
 	if '>' in line:
-		seqID.append(line)
+		seqID.append(line[1:])
 	else:
 		Slength=float(len(line))
 		Gcount=line.count("G")
 		Ccount=line.count("C")
-	seqlength.append(Slength)
-	percentGC.append((Gcount+Ccount)/Slength*100)
-
+		seqlength.append(Slength)
+		percentGC.append(float(Gcount+Ccount)/Slength*100)
+print (seqID)
+print (percentGC)
+print (seqlength)
 # make lists into a dataframe that you can use to plot the histogram
 data=pandas.DataFrame(list(zip(seqID,percentGC,seqlength)),columns=['seqID','seqlength','percentGC'])
+print (data)
